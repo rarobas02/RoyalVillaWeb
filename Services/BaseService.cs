569df4ba4fdbb6d4d189c1f5f17a7fba20492ae9
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-12 21:33:18 +0800

    69. Base Service Interface

---
+++ Services/BaseService.cs
@@ -0,0 +1,21 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Services
+{
+    public class BaseService : IBaseService
+    {
+        public IHttpClientFactory httpClient { get; set; }
+        public ApiResponse<object> ResponseModel { get; set; }
+        public BaseService(IHttpClientFactory httpClient)
+        {
+            this.ResponseModel = new();
+            this.httpClient = httpClient;
+        }
+        public Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
