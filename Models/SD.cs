45b31e8c89894d4e6269a78cd74a1bedffd3f6b1
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-12 21:22:35 +0800

    68. API Request Model

---
+++ Models/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace RoyalVillaWeb.Models
+{
+    public static class SD
+    {
+        public enum ApiType
+        { 
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
