cb218f0be77624f1549be9d1f44c15c13b5618ba
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-12 21:58:20 +0800

    71. Villa Service Interface

---
+++ Services/VillaService.cs
@@ -0,0 +1,33 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Services
+{
+    public class VillaService : IVillaService
+    {
+        public Task<T?> CreateAsync<T>(VillaCreateDTO dto, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> DeleteAsync<T>(int id, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAllAsync<T>(string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> GetAsync<T>(int id, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> UpdateAsync<T>(VillaUpdateDTO dto, string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
