1b570e48c1a2941b9a3c2d3e4cd25828dfeae76a
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-05-18 21:36:50 +0800

    80. Villa management in navbar

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,37 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
+using RoyalVillaWeb.Models;
+using RoyalVillaWeb.Services.IServices;
+using System.Diagnostics;
+
+namespace RoyalVillaWeb.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaService _villaService;
+        private readonly IMapper _mapper;
+        public VillaController(IVillaService villaService, IMapper mapper)
+        {
+            _villaService = villaService;
+            _mapper = mapper;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<VillaDTO> villaList = new();
+            try
+            {
+                var response = await _villaService.GetAllAsync<ApiResponse<List<VillaDTO>>>(""); //the blank string in the parameter is for the token,
+                if(response is not null && response.Data is not null)
+                {
+                    villaList = response.Data;
+                }
+            }
+             catch (Exception ex)
+            {
+                TempData["error"] = $"An error occured: {ex.Message}";
+            }
+            return View(villaList);
+        }
+    }
+}
