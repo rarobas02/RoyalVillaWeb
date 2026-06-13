d2b2ddcf7f077dbfd4b65addef73627545f7c79c
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-06-13 20:50:15 +0800

    89. Auth Service Interface

---
+++ Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using RoyalVilla.DTO;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Services
+{
+    public class AuthService : BaseService, IAuthService
+    {
+        private const string APIEndpoint = "/api/auth";
+        public AuthService(IHttpClientFactory httpClient, IConfiguration configuration) : base(httpClient)
+        {
+        }
+
+        public Task<T?> LoginAsync<T>(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T?> RegisterAsync<T>(RegistrationRequestDTO registerRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/IServices/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using RoyalVilla.DTO;
+
+namespace RoyalVillaWeb.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<T?> LoginAsync<T>(LoginRequestDTO loginRequestDTO);
+        Task<T?> RegisterAsync<T>(RegistrationRequestDTO registerRequestDTO);
+    }
+}
