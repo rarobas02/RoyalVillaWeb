15c14e8487c0f431e408c36e44bdfa4353181e0d
Remark A. Robas <remarkarobas@iskolarngbayan.pup.edu.ph>
2026-06-15 21:43:17 +0800

    91. Auth Controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using RoyalVilla.DTO;
+using RoyalVillaWeb.Services.IServices;
+
+namespace RoyalVillaWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly IMapper _mapper;
+        public AuthController(IAuthService authService, IMapper mapper)
+        {
+            _authService = authService;
+            _mapper = mapper;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
+        {
+            try
+            {
+                var response = await _authService.LoginAsync<ApiResponse<LoginResponseDTO>>(loginRequestDTO); //the blank string in the parameter is for the token,
+                if (response is not null && response.Data is not null)
+                {
+                    LoginResponseDTO loginResponse = response.Data;
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["error"] = $"An error occured: {ex.Message}";
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegistrationRequestDTO { 
+                Email = string.Empty,
+                Name = string.Empty,
+                Password = string.Empty,
+                Role = "Customer"
+            });
+        }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+    }
+}
